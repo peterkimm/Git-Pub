2d192c10b5884341d642517adb11aff7c58ff878
Peter <peterkimm35@gmail.com>
2022-05-17 13:46:12 -0700

    Welcome to the Gitpub App!

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the Gitpub App!");
+});
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Listening on port`, port)
+});
